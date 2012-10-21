77dbf669f9751363ab24cdfdfd1f5a79eb5470d5
Dmitry Vakhrushev <self@kr41.net>
2012-10-21 13:09:54 +0700

    Added source code

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+import greenrocket
+
+setup(
+    name='GreenRocket',
+    version=greenrocket.__version__,
+    description="A simple implementation of Observer pattern via Signals",
+    long_description=greenrocket.__doc__,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='',
+    author='Dmitry Vakhrushev',
+    author_email='self@kr41.net',
+    url='',
+    license='BSD',
+    packages=['greenrocket'],
+    include_package_data=True,
+    zip_safe=True,
+)
